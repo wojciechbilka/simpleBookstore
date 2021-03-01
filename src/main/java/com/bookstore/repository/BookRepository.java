b6ad037d2791950de6941223f305b272d5a814bb
wojciechbilka <wojciech.bilka@gmail.com>
2021-03-01 17:22:17 +0100

    Added findByIdAndBorrowerIsNotNull method to BookRepository

--- src/main/java/com/bookstore/repository/BookRepository.java
+++ src/main/java/com/bookstore/repository/BookRepository.java
@@ -1,12 +1,14 @@
 package com.bookstore.repository;
 
 import com.bookstore.model.Book;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
@@ -18,4 +20,6 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Query("SELECT u FROM Book u WHERE u.author = :author")
     List<Book> findByAuthor(@Param("author") String author);
+
+    Optional<Book> findByIdAndBorrowerIsNotNull(Long id);
 }
