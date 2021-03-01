d4e783ee4de46bcfee915fa34723fdc64d5eba66
wojciechbilka <wojciech.bilka@gmail.com>
2021-03-01 17:38:09 +0100

    Modified SecurityConfig for UserDetails setup

--- src/main/java/com/bookstore/config/SecurityConfig.java
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -1,21 +1,44 @@
 package com.bookstore.config;
 
+import com.bookstore.service.UserBookstoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    final private UserBookstoreService userBookstoreService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userBookstoreService).passwordEncoder(getPasswordEncoder());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests().anyRequest()
-                .permitAll();
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
+        http.authorizeRequests()
+                .antMatchers("/**").hasRole("USER")
+                //.antMatchers("/bookList").permitAll()
+                .and()
+                .formLogin()
+                //.formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
 
