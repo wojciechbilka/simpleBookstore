7b11871b3c7c333728186d593e6efbf27838238d
wojciechbilka <wojciech.bilka@gmail.com>
2021-03-01 17:26:04 +0100

    Added returnBook, isBorrowed and deleteById methods in BookService

--- src/main/java/com/bookstore/service/BookService.java
+++ src/main/java/com/bookstore/service/BookService.java
@@ -1,7 +1,9 @@
 package com.bookstore.service;
 
 import com.bookstore.model.Book;
+import com.bookstore.model.Person;
 import com.bookstore.repository.BookRepository;
+import com.bookstore.repository.PersonRepository;
 import lombok.Data;
 
 import org.springframework.stereotype.Service;
@@ -14,6 +16,7 @@ import java.util.Optional;
 public class BookService {
 
     private final BookRepository bookRepository;
+    private final PersonRepository personRepository;
 
     public List<Book> getAllBooks() {
         return bookRepository.findByOrderByTittleAsc();
@@ -33,5 +36,30 @@ public class BookService {
 
     public void save(Book book) {
         bookRepository.save(book);
+    }
+
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    public boolean isBorrowed(Long id) {
+        Optional<Book> book = bookRepository.findByIdAndBorrowerIsNotNull(id);
+        return book.isPresent();
+    }
+
+    public void returnBook(Long id) {
+        Optional<Book> optionalBook = getById(id);
+        System.out.println(optionalBook);
+        optionalBook.ifPresent(book -> {
+            if(book.isBorrowed()) {
+                Person person = book.getBorrower();
+                person.getBooks().remove(book);
+                personRepository.save(person);
+                book.setBorrower(null);
+                book.setAvailable(true);
+                book.setBorrowingDate(null);
+                bookRepository.save(book);
+            }
+        });
     }
 }
