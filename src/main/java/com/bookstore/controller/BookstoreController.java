82097368d78ee36385d2ef2acc4727f565147aff
wojciechbilka <wojciech.bilka@gmail.com>
2021-03-01 17:42:36 +0100

    Added confirmReturnBook, myBooks, register and saveUserBookstore methods to BookstoreController

--- src/main/java/com/bookstore/controller/BookstoreController.java
+++ src/main/java/com/bookstore/controller/BookstoreController.java
@@ -1,7 +1,12 @@
 package com.bookstore.controller;
 
+import com.bookstore.model.Address;
 import com.bookstore.model.Book;
+import com.bookstore.model.Person;
+import com.bookstore.model.UserBookstore;
 import com.bookstore.service.BookService;
+import com.bookstore.service.PersonService;
+import com.bookstore.service.UserBookstoreService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
@@ -10,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,11 +25,18 @@ import java.util.List;
 public class BookstoreController {
 
     private final BookService bookService;
+    private final UserBookstoreService userBookstoreService;
+    private final PersonService personService;
 
     @GetMapping("/")
     public String index(Model model, Authentication authentication) {
         List<Book> books = bookService.getAllBooks();
         model.addAttribute("books", books);
+
+        UserBookstore user = (UserBookstore) authentication.getPrincipal();
+        Integer booksCount = user.getPerson().getBooks().size();
+        model.addAttribute("personId", user.getPerson().getId());
+        model.addAttribute("booksCount", booksCount);
         //TODO Add pagination for the list
         return "shelf/bookList";
     }
@@ -41,6 +54,45 @@ public class BookstoreController {
     @PostMapping("/saveBook")
     public String saveBook(@ModelAttribute Book book, Authentication authentication) {
         bookService.save(book);
+        return "redirect:/";
+    }
+
+    @PostMapping("/deleteBook")
+    public String deleteBook(@RequestParam("book_id") Long bookId, Authentication authentication) {
+        if(!bookService.isBorrowed(bookId)) {
+            bookService.deleteById(bookId);
+        }
+        return "redirect:/";
+    }
+
+    @PostMapping("/confirmReturnBook")
+    public String confirmReturnBook(@RequestParam("book_id") Long bookId, Authentication authentication) {
+        bookService.returnBook(bookId);
+        return "redirect:/";
+    }
+
+    @GetMapping("/myBooks")
+    public String myBooks(Model model, Authentication authentication) {
+        UserBookstore user = (UserBookstore) authentication.getPrincipal();
+        List<Book> books = user.getPerson().getBooks();
+        model.addAttribute("myBooks", books);
+        return "shelf/my_books";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model, Authentication authentication) {
+        UserBookstore userBookstore = new UserBookstore();
+        Person person = new Person();
+        person.setAddress(new Address());
+        userBookstore.setPerson(person);
+        model.addAttribute("userBookstore", userBookstore);
+        return "/registration_form";
+    }
+
+    @PostMapping("/saveUserBookstore")
+    public String saveUserBookstore(@ModelAttribute UserBookstore userBookstore, Authentication authentication) {
+        userBookstoreService.save(userBookstore);
+        System.out.println(userBookstore);
         return "redirect:/";
     }
 }
